267160d0430c8b852ae8ecc633d9fb91af966731
garima <garima.sanghai@gmail.com>
2024-10-14 22:35:51 +0530

    new code changes again

--- src/main/java/ATBOct2024/lab_006.java
+++ src/main/java/ATBOct2024/lab_006.java
@@ -2,6 +2,17 @@ package ATBOct2024;
 
 public class lab_006 {
     public static void main(String[] args) {
-        System.out.println("I hope i  learn quick");
+        int num = 9;
+        //souf for printf - shorcut
+        System.out.println("Table of 9");
+        System.out.printf("%d x 1 = %d", num, num * 1);
+        System.out.printf("\n");
+        System.out.printf("%d x 2 = %d", num, num * 2);
+        System.out.printf("\n");
+        System.out.printf("%d x 3 = %d", num, num * 3);
+        System.out.printf("\n");
+        System.out.printf("%d x 4 = %d", num, num * 4);
+        System.out.printf("\n");
+        System.out.printf("%d x 5 = %d", num, num * 5);
     }
 }

39b8deb12c286990f0d8092b1903c376ebd91bf0
garima <garima.sanghai@gmail.com>
2024-10-15 21:57:43 +0530

    new code changes

---
+++ src/main/java/ATBOct2024/lab_013.java
@@ -0,0 +1,11 @@
+package ATBOct2024;
+
+public class lab_013 {
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(++a + a++ + a++);
+        // 11 + 11 + 12 = 34
+        System.out.println(a);
+        // 13
+    }
+}
---
+++ src/main/java/ATBOct2024/lab_014.java
@@ -0,0 +1,13 @@
+package ATBOct2024;
+
+public class lab_014 {
+    public static void main(String[] args) {
+        int a = 20;
+
+        System.out.println(-- a + a++ + a --);
+        // 19 + 19 + 20 = 58
+        System.out.println(a);
+        // 19
+
+    }
+}

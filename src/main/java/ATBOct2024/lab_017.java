c6cf858c8552c68fa1fd524ffb842039fb78f5ef
garima <garima.sanghai@gmail.com>
2024-10-16 08:29:14 +0530

    new code changes

--- src/main/java/ATBOct2024/lab_017.java
+++ src/main/java/ATBOct2024/lab_017.java
@@ -6,11 +6,12 @@ public class lab_017 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Name: ");
-        System.out.println("Age: ");
-        System.out.println("Salary: ");
         String username = sc.next();
+        System.out.println("Age: ");
         int age = sc.nextInt();
+        System.out.println("Salary: ");
         int salary = sc.nextInt();
+        System.out.println("User details given");
         System.out.println("My name is : " + username);
         System.out.println("My age is : " + age );
         System.out.println("My salary is : " + salary);
--- src/main/java/ATBOct2024/lab_018.java
+++ src/main/java/ATBOct2024/lab_018.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 public class lab_018 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter first num");
-        System.out.println("Enter second num");
+        System.out.println("Enter first num: ");
         int a = sc.nextInt();
+        System.out.println("Enter second num: ");
         int b = sc.nextInt();
         int maxVal = (a>b ? a:b);
         System.out.println("The maximum value is: " + maxVal);

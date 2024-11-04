27e823c5637e1074e7b731e841c13c331cf3e3e8
garima <garima.sanghai@gmail.com>
2024-11-05 01:09:24 +0530

    new code changes

--- src/main/java/Array/lab_062arrayRightAngleTriangle.java
+++ src/main/java/Array/lab_062arrayRightAngleTriangle.java
@@ -4,7 +4,7 @@ public class lab_062arrayRightAngleTriangle {
     public static void main(String[] args) {
         String [][] matrix = {{"*"}, {"*","*"}, {"*","*","*"},  {"*","*","*","*"}};
         for(int i = 0; i<matrix.length; i++){
-            for(int j = 0; j<matrix[i].length; j++) {
+            for(int j = 0; j<matrix[i].length; j++) { // will always give u columns
                     System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
--- src/main/java/Array/lab_064arrayRectangle.java
+++ src/main/java/Array/lab_064arrayRectangle.java
@@ -2,15 +2,15 @@ package Array;
 
 import java.util.Scanner;
 
-public class lab_063arrayLeftAngleTriangle {
+public class lab_064arrayRectangle {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the size of rows");
         int size = sc.nextInt();
-        int [] triangleSize = new int[size];
+        int [] rectangleSize = new int[size];
 
-        for(int i = 0; i < triangleSize.length; i++){
-            for(int j = i; j < triangleSize.length; j++) { // will always give u columns
+        for(int i = 0; i < rectangleSize.length; i++){
+            for(int j = 0; j < rectangleSize.length; j++) { // will always give u columns
                 System.out.print("*" + " ");
             }
             System.out.println();

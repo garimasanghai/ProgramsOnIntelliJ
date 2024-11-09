e301831d7649b4f46fe261dd5890297f15de2ff5
garima <garima.sanghai@gmail.com>
2024-11-09 11:30:00 +0530

    new code changes

--- src/main/java/Array/lab_071starPyramidTriangle.java
+++ src/main/java/Array/lab_071starPyramidTriangle.java
@@ -1,16 +1,18 @@
 package Array;
 
-public class lab_071Triangle {
+public class lab_071starPyramidTriangle {
     public static void main(String[] args) {
         for( int i = 1; i <=4; i ++){ //This will be constant for all prog in start pattern
             for(int j=4; j>=i; j--){
                 System.out.print(" ");
             }
             for(int k = 1; k<=i; k++){
-                System.out.print("*" + " ");
+                //System.out.print("*" + " "); OR
+                //System.out.print(" " + "*"); OR
+                System.out.print(" *");
             }
             System.out.println();
         }
-        System.out.println("This is a Triangle");
+        System.out.println("This is a Star Pyramid Triangle");
     }
 }

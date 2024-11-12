7a6a92b3fca6960532cb0edd409b063eef49b89a
garima <garima.sanghai@gmail.com>
2024-11-12 05:46:34 +0530

    new code changes

---
+++ src/main/java/Encapsulation/lab_081Demo1.java
@@ -0,0 +1,35 @@
+package Encapsulation;
+
+public class lab_081Demo1 {
+   /* public class Thief {
+        public static void main(String[] args) {
+
+            Cop c = new Cop(10);
+            //System.out.println(c.gun);
+            //c.canIShoot();
+}
+    }
+    */
+
+
+   /*public class Cop {
+
+       private int gun;
+       public String iCard;
+
+       public Cop(int gun){
+           this.gun = gun;
+       }
+
+       protected void canIShoot(){
+           System.out.println("Yes, You can!!");
+       }
+       */
+
+   /* public class JrCop {
+        public static void main(String[] args) {
+            Cop jrCop = new Cop(5);
+            // System.out.println(jrCop.gun);
+            jrCop.canIShoot();
+        }*/
+}

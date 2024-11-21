190efc50816eb53ed3b12f698a7b51ff4211a6c8
garima <garima.sanghai@gmail.com>
2024-11-21 23:01:00 +0530

    new code changes

---
+++ src/main/java/JavaInterviewPrograms/Test1.java
@@ -0,0 +1,13 @@
+package JavaInterviewPrograms;
+
+public class Test1 {
+    public static void func(int a, int b){
+        System.out.println(a+ "" + b);
+    }
+    public static void main(String[] args) {
+     int a= 5;
+     int b = 7;
+     func(a,b);
+
+    }
+}
